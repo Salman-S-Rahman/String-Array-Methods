bcc453631dc453c7d86413b1d2d8c02541021dc5
Salman-S-Rahman <salmansr.developer@gmail.com>
2021-12-16 22:43:45 +0600

    file added

---
+++ split.js
@@ -0,0 +1,4 @@
+var text = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content."
+
+const words = text.split(' ')
+console.log(words)
